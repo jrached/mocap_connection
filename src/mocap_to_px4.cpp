c3599a5e5f79f5847c398253d70cbc5826cfb223
Juan Rached <juanrached@gmail.com>
2024-10-18 16:12:49 -0400

    Send Heartbeat

--- src/mocap_to_px4.cpp
+++ src/mocap_to_px4.cpp
@@ -4,6 +4,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include <px4_msgs/msg/vehicle_odometry.hpp>
+//#include <px4_msgs/msg/offboard_control_mode.hpp>
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -17,7 +18,9 @@ class MocapConnection : public rclcpp::Node
         mocap_subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/PX01/world", 10, std::bind(&MocapConnection::mocap_callback, this, _1));
         px4_publisher_ = this->create_publisher<px4_msgs::msg::VehicleOdometry>("/fmu/in/vehicle_visual_odometry", 10); 
 
-        // Make timer for 40 Hz
+        //offboard_control_mode_publisher_ = this->create_publisher<px4_msgs::msg::OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
+	
+	// Make timer for 40 Hz
         timer_ = this->create_wall_timer(25ms, std::bind(&MocapConnection::timer_callback, this));
     }
 
@@ -25,6 +28,7 @@ class MocapConnection : public rclcpp::Node
 
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr mocap_subscription_;
     rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr px4_publisher_;
+    //rclcpp::Publisher<px4_msgs::msg::OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
 
     // Declare timer and pose message
     rclcpp::TimerBase::SharedPtr timer_;
@@ -38,7 +42,8 @@ class MocapConnection : public rclcpp::Node
     void timer_callback(){
         // Publish mocap data under new topic at 40 Hz
         auto odom_msg = px4_msgs::msg::VehicleOdometry();
-        odom_msg.timestamp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
+        
+	odom_msg.timestamp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
         odom_msg.position[0] =pose_msg.pose.position.y;
         odom_msg.position[1] =pose_msg.pose.position.x;
         odom_msg.position[2] = -pose_msg.pose.position.z;
@@ -48,6 +53,17 @@ class MocapConnection : public rclcpp::Node
         odom_msg.q[3] = -pose_msg.pose.orientation.z;
 
         this->px4_publisher_->publish(odom_msg);
+
+	// Publish heartbeat
+	//px4_msgs::msg::OffboardControlMode msg{};
+	//msg.position = true;
+	//msg.velocity = false;
+	//msg.acceleration = false;
+	//msg.attitude = false;
+	//msg.body_rate = false;
+	//msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+	//offboard_control_mode_publisher_->publish(msg);
+
     }
 
 
